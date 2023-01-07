4b754a018b6466a24a24e26e555f6cffc023b448
shanksh <shadowthedark20@gmail.com>
2023-01-07 22:18:01 +0200

    removed pdf creator

--- MyProject/Source.cpp
+++ MyProject/Source.cpp
@@ -2,49 +2,63 @@
 #include "Bilet.h"
 #include "Eveniment.h"
 #include "Locatie.h"
+#include <Aspose.PDF.Cpp/Document.h>
+#include <Aspose.PDF.Cpp/Page.h>
+#include <Aspose.PDF.Cpp/PageCollection.h>
+#include <Aspose.PDF.Cpp/Color.h>
+#include <Aspose.PDF.Cpp/Text/TextFragment.h>
+#include <Aspose.PDF.Cpp/Text/Position.h>
+#include <Aspose.PDF.Cpp/Text/TextBuilder.h>
+#include <Aspose.PDF.Cpp/Text/TextFragmentCollection.h>
+#include <Aspose.PDF.Cpp/Text/TextFragmentState.h>
+#include <Aspose.PDF.Cpp/Text/FontRepository.h>
 
-int main()
+using namespace Aspose::Pdf;
+using namespace Aspose::Pdf::Text;
+
+int main(int argc, char* argv[])
 {
-	Date d(20, 2, 2023);
-	//std::cout << d.DateFormatted('-');
+	std::string currentPath = argv[0];
+	currentPath.resize(currentPath.find_last_of("\\/"));
+	std::cout << "Current path: " << currentPath << std::endl;
 
-	Time t(23, 59);
-	//std::cout << t.timeFormatted(':');
+	//Date d(20, 2, 2023);
+	////std::cout << d.DateFormatted('-');
 
-	Locatie l("testttt", 5, 4);
+	//Time t(23, 59);
+	////std::cout << t.timeFormatted(':');
 
-	Eveniment eveniment_cool((const char*)"testttttt", 2, l, t, d);
+	//Locatie l("testttt", 5, 4);
 
-	/*std::cout << eveniment_cool << std::endl;
+	//Eveniment eveniment_cool((const char*)"testttttt", 2, l, t, d);
 
-	++eveniment_cool;
+	///*std::cout << eveniment_cool << std::endl;
 
-	std::cout << eveniment_cool << std::endl;*/
+	//++eveniment_cool;
 
-	//std::cout << eveniment_cool.getLocatie()[1][2];
+	//std::cout << eveniment_cool << std::endl;*/
 
-	Bilet b(eveniment_cool);
+	////std::cout << eveniment_cool.getLocatie()[1][2];
 
-	l + 2;
+	//Bilet b(eveniment_cool);
 
-	unsigned* test = new unsigned[2];
-	test[0] = 2;
-	test[1] = 4;
+	//l + 2;
 
-	l.setRanduriVip(test, 2);
+	//unsigned* test = new unsigned[2];
+	//test[0] = 2;
+	//test[1] = 4;
 
-	system("CLS");
+	//l.setRanduriVip(test, 2);
 
-	std::cin >> l;
+	////system("CLS");
 
-	std::cin >> b;
+	////std::cin >> l;
 
-	std::cout << b;
+	///*std::cin >> b;
 
-	b.renuntareBilet();
+	//std::cout << b;*/
 
-	std::cout << b;
-
+	////b.renuntareBilet();
 
 	return 0;
 }
